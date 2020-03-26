aa6b3042287aaa9c455bc8b63a3a61766967c5db
Jianghaonan Wang <jianghaonanwang@Jianghaonans-MacBook-Pro.local>
2020-03-26 18:31:45 -0400

    chatapp update

---
+++ app.js
@@ -0,0 +1,50 @@
+var express = require("express");
+var app = express();
+
+// add socket here
+const io = require("socket.io")();
+
+const port = process.env.PORT || 3030;
+
+// tell express where our static files are (js, images, css etc)
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
+});
+
+const server = app.listen(port, () => {
+  console.log(`app is running on port ${port}`);
+});
+
+// attach our chat server to our app
+io.attach(server);
+
+const nicknameList = {};
+
+io.on("connection", function(socket) {
+  // socket is your connection
+  console.log(" a user has connected");
+  socket.emit("connected", { sID: socket.id, message: "new connection" });
+
+  socket.on("new_user", function(name) {
+    nicknameList[socket.id] = name;
+    io.emit("user-connected", name);
+    io.emit("get-users", nicknameList);
+  });
+
+  socket.on("chat_message", function(msg) {
+    console.log(msg); // let's see what the payload is from the client side
+
+    // tell the connection manager (socket.io) to send this message to everyone
+    // anyone connected to our chat app will get this message(including the sender)
+    io.emit("new_message", { id: socket.id, message: msg });
+  });
+
+  socket.on("disconnect", function() {
+    console.log("a user has disconnected");
+    io.emit("user-disconnected", nicknameList[socket.id]);
+    delete nicknameList[socket.id];
+    io.emit("get-users", nicknameList);
+  });
+});
